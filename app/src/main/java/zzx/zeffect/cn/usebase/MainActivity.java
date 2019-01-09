3af27d8596dd50d1187e997430aa804038d9e390
zeffect <625417632@qq.com>
2019-01-09 11:32:46 +0800

    提交一些测试

--- app/src/main/java/zzx/zeffect/cn/usebase/MainActivity.java
+++ app/src/main/java/zzx/zeffect/cn/usebase/MainActivity.java
@@ -8,7 +8,13 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.TextView;
 
+import zzx.zeffect.cn.usebaselib.anim.AnimUtils;
+import zzx.zeffect.cn.usebaselib.device.DeviceUtils;
+import zzx.zeffect.cn.usebaselib.encrypt.Md5Utils;
+import zzx.zeffect.cn.usebaselib.meta.MetaUtils;
+import zzx.zeffect.cn.usebaselib.network.NetUtils;
 import zzx.zeffect.cn.usebaselib.permission.PermissionUtils;
+import zzx.zeffect.cn.usebaselib.screen.ScreenUtils;
 import zzx.zeffect.cn.usebaselib.system.SystemUtils;
 
 public class MainActivity extends Activity implements View.OnClickListener {
@@ -27,6 +33,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
         findViewById(R.id.raiseVolumeBtn).setOnClickListener(this);
         findViewById(R.id.lowerBrightBtn).setOnClickListener(this);
         findViewById(R.id.lowerVolumeBtn).setOnClickListener(this);
+        findViewById(R.id.anim_btn).setOnClickListener(this);
+        findViewById(R.id.toErrorBtn).setOnClickListener(this);
     }
 
 
@@ -56,6 +64,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
             SystemUtils.maxVolume(this);
         } else if (v.getId() == R.id.setMinVolumeBtn) {
             SystemUtils.minVolume(this);
+        } else if (v.getId() == R.id.anim_btn) {
+            AnimUtils.jitter(v);
+        } else if (v.getId() == R.id.toErrorBtn) {
+            String tmpStr = null;
+            tmpStr.trim();
         }
     }
 
@@ -63,7 +76,17 @@ public class MainActivity extends Activity implements View.OnClickListener {
     private void testAll() {
         showMsg("当前电量：" + SystemUtils.getCurBattery(this));
         showMsg("屏幕亮度：" + SystemUtils.getWindowBrightness(this));
-
-
+        showMsg("IMEI:" + DeviceUtils.getImei(this));
+        showMsg("MAC:" + DeviceUtils.getMac(this));
+        showMsg("android id:" + DeviceUtils.getAndroidID(this));
+        showMsg("android type:" + DeviceUtils.getAndroidType());
+        showMsg("md5 123456=" + Md5Utils.md5("123456"));
+        showMsg("meta data key1:" + MetaUtils.getMetaData(this, "key1", ""));
+        showMsg("meta data key2:" + MetaUtils.getMetaData(this, "key2", 0));
+        showMsg("net 是否连接:" + NetUtils.isConnected(this));
+        showMsg("net 是否WIFI:" + NetUtils.hasWifi(this));
+        showMsg("net 是否手机：" + NetUtils.isMobile(this));
+        showMsg("是否平板："+ ScreenUtils.isPad(this));
+        showMsg("是否横屏："+ScreenUtils.isLand(this));
     }
 }
