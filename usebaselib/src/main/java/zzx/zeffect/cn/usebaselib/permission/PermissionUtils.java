12778a6429d13e22a2e8d977285aa118e3d91ade
zeffect <625417632@qq.com>
2019-01-10 18:22:59 +0800

    悬浮窗权限

--- usebaselib/src/main/java/zzx/zeffect/cn/usebaselib/permission/PermissionUtils.java
+++ usebaselib/src/main/java/zzx/zeffect/cn/usebaselib/permission/PermissionUtils.java
@@ -1,9 +1,17 @@
 package zzx.zeffect.cn.usebaselib.permission;
 
+import android.app.Activity;
+import android.app.AppOpsManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Binder;
 import android.os.Build;
+import android.provider.Settings;
 import android.text.TextUtils;
+
+import java.lang.reflect.Method;
 
 /**
  * Created by zeffect on 18-12-12.
@@ -48,4 +56,37 @@ public class PermissionUtils {
         }
         return true;
     }
+
+
+    public static boolean hasAlertPermission(Context pContext) {
+        try {
+            Object object = pContext.getSystemService(Context.APP_OPS_SERVICE);
+            if (object == null) {
+                return false;
+            }
+            Class localClass = object.getClass();
+            Class[] arrayOfClass = new Class[3];
+            arrayOfClass[0] = Integer.TYPE;
+            arrayOfClass[1] = Integer.TYPE;
+            arrayOfClass[2] = String.class;
+            Method method = localClass.getMethod("checkOp", arrayOfClass);
+            if (method == null) {
+                return false;
+            }
+            Object[] arrayOfObject1 = new Object[3];
+            arrayOfObject1[0] = Integer.valueOf(24);
+            arrayOfObject1[1] = Integer.valueOf(Binder.getCallingUid());
+            arrayOfObject1[2] = pContext.getPackageName();
+            int m = ((Integer) method.invoke(object, arrayOfObject1)).intValue();
+            return m == AppOpsManager.MODE_ALLOWED;
+        } catch (Exception ex) {
+        }
+        return false;
+    }
+
+
+    public static void toAlertPermission(Activity activity, int requestCode) {
+        activity.startActivityForResult(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + activity.getPackageName())), requestCode);
+    }
+
 }
